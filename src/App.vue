9f9d19eea601a8bf27107a0242cf075c3bfa1f05
Ghassan Al Karaan <ghass.karaan@gmail.com>
2024-07-19 11:27:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+
+
+</script>
+<template>
+  <Navbar />
+  <Hero title="Hi bro shou el a5bar" subtitle="Baddak Job?" />
+  <HomeCards/>
+</template>
